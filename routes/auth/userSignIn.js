8271108d5e1220f5db8e532d6ffa53715eee6338
Mia Zhang <miyawingz@gmail.com>
2020-02-22 11:25:30 +0000

    added token expiration

--- routes/auth/userSignIn.js
+++ routes/auth/userSignIn.js
@@ -4,16 +4,21 @@ const queries = require('../../queries');
 const { queryAsync } = require('../../db');
 const { compare } = require('../../lib/hash');
 const { tokenHandler } = require('../../middleware/token_handler');
-const { tokenEncode } = require('../../lib/jwtHandler');
+const { tokenEncode, tokenDecode } = require('../../lib/jwtHandler');
 const { emailValidate } = require('../../lib/userUtils');
 const ApiError = require('../../lib/apiError');
 
 router.get('/', tokenHandler, async (req, res, next) => {
-    const { uid } = res.locals;
+    const { uid, token } = res.locals;
 
     try {
-        if (!uid) {
+        if (!uid || !token) {
             return next(new ApiError(400, 'invalid user token'))
+        }
+
+        const decode = tokenDecode(token);
+        if (Date.now() > decode.exp) {
+            return next(new ApiError(500, 'token has expired'))
         }
 
         const queryInfo = queries.GetUserByUid(uid);
@@ -34,8 +39,8 @@ router.post('/', tokenHandler, async (req, res, next) => {
     const { email, password } = req.body;
 
     try {
-        if (!emailValidate(email)) {
-            return next(new ApiError(422, 'invalid email'));
+        if (!password || !email || !emailValidate(email)) {
+            return next(new ApiError(422, 'invalid email or missing password'));
         }
 
         const queryInfo = queries.GetUserByEmail(email);
@@ -52,14 +57,14 @@ router.post('/', tokenHandler, async (req, res, next) => {
 
         if (cartId) {
             const queryInfo = queries.UpdateCartUserId(pid, cartId);
-            const result = await queryAsync(queryInfo.text, queryInfo.values);
+            await queryAsync(queryInfo.text, queryInfo.values);
         }
 
-        const token = tokenEncode({ uid: pid });
+        const token = tokenEncode({ uid: pid, iat: Date.now(), exp: (Date.now() + 12096e5) });
 
         res.send({
             token,
-            'user': {
+            user: {
                 email, name, pid
             }
         })
